376fd73e71e4c65bf6a989d8ba510ac890538ff0
Roman_Bryzgalin <rbryzgalin@gmail.com>
2023-11-06 16:56:19 +0700

    нужно менять на просто массив и делать через for

---
+++ script.js
@@ -0,0 +1,26 @@
+const navInfo = [
+    {
+        image: "./images/slider_photo_1.png",
+        link: "Строка навигации 1"
+    },
+    {
+        image: "./images/slider_photo_2.png",
+        link: "Строка навигации 2"
+    },
+    {
+        image: "./images/slider_photo_3.png",
+        link: "Строка навигации 3"
+    }
+]
+
+let sliderImage = document.querySelector(".picture");
+console.log(sliderImage);
+let button_prev = document.querySelector(".button_prev");
+console.log(button_prev);
+let button_next = document.querySelector(".button_next");
+console.log(button_next);
+
+
+let nav = document.querySelector(".li");
+console.log(nav);
+
